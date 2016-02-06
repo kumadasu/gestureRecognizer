fff2f552e82dc879919c28993699442c26873372
kumadasu <kumadasu@gmail.com>
2016-02-07 00:46:08 +0900

    Projection to plane.

--- receive.py
+++ receive.py
@@ -1,10 +1,25 @@
 import serial
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+
+
+def projection_to_plane(hpb):
+    h = hpb[0]/360*2*math.pi
+    p = hpb[1]/360*2*math.pi
+    return [math.tan(h), math.tan(-p)/math.cos(h)]
+
+fig, ax = plt.subplots()
+
+x = []
+y = []
+
+points, = ax.plot(x, y, marker='o', linestyle='--')
+size = 5
+ax.set_xlim(-size, size)
+ax.set_ylim(-size, size)
 
 with serial.Serial('/dev/ttyUSB0', 115200, timeout=3) as ser:
-    hpb = [[0, 0, 0]]
-
     for num in range(1,5000):
         line = ser.readline()
 
@@ -24,8 +39,10 @@ with serial.Serial('/dev/ttyUSB0', 115200, timeout=3) as ser:
         (header, h, p, b) = [t(s) for t, s in zip((str, float, float, float), line.strip(b'\r\n').split(b'\t'))]
         print(header, h, p, b)
 
-        hpb = np.vstack([hpb, [h, p, b]])
-        if num % 100 == 0 :
-            plt.cla
-            plt.plot(hpb)
+        hpb = [h, p, b]
+        x_coord, y_coord = projection_to_plane(hpb)
+        x.append(x_coord)
+        y.append(y_coord)
+        points.set_data(x, y)
+        if num % 20 == 0 :
             plt.pause(0.0000001)
